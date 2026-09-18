7467208cf6f04bdcb674e86f59254a572a24eefd
suvendusahani145-wq <suvendusahani145@gmail.com>
2026-09-18 15:38:10 +0530

    Update Staging_STOCK_TRADES.sql with incremental model configuration

--- stock-data-pipeline/stock-dbt/stock_dbt/models/Staging/Staging_STOCK_TRADES.sql
+++ stock-data-pipeline/stock-dbt/stock_dbt/models/Staging/Staging_STOCK_TRADES.sql
@@ -1,12 +1,15 @@
-{{% config(materialized='Table',
+{{% config(materialized='incremental',
             schema= 'Bronze',
+            unique_key= TRADE_TIMESTAMP_UTC,
+            incremtal_startegy= 'merge'
         )%}}
 
 select ID,
        TYPE,
        SYMBOL,
-       PRICE,
+       cast(PRICE as NUMBER(15,4))as PRICE,
        VOLUME,
+       PRICE * VOLUME AS TRADE_VALUE,
        TRADE_TIMESTAMP,
        TRADE_CONDITIONS,
        case when TRADE_CONDITIONS=1 then "RRGULAR"
@@ -14,3 +17,7 @@ select ID,
             WHEN TRADE_CONDITIONS=24 then "EXTENDED_HOURS"
             END AS TRADE_CONDITION_GROUP
     from {{ref('Bronze_STOCK_TRADES')}}
+
+{{if is_incremental()%}}
+where TRADE_TIMESTAMP > (select TRADE_TIMESTAMP from {{this}})
+
